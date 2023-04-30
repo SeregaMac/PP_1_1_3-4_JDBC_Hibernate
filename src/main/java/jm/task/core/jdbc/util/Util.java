bc9335bef3161d302be66e06fd07abc331714a11
Sergey_Makarov <seregamac@mail.ru>
2023-04-30 18:45:28 +0300

    Реализовал class Util.

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,26 @@
 package jm.task.core.jdbc.util;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Util {
+    public static final String DB_URL = "jdbc:mysql://localhost:3306";
+    public static final String DB_USERNAME = "root";
+    public static final String DB_PASSWORD = "Lesnica20122012";
+
+    public Connection getConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+            System.out.println("Connection OK");
+
+        } catch ( SQLException e) {
+            e.printStackTrace();
+            System.out.println("Connection ERROR");
+        }
+        System.out.println("Удачно");
+        return connection;
+    }
     // реализуйте настройку соеденения с БД
 }
+
